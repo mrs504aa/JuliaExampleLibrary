eb29510954f34ba879d07cb781c915593971ee94
Ruosong Mao <mrs504aa@hotmail.com>
2022-05-22 18:18:13 +0800

    Update

---
+++ ChannelAndSpawn/ExampleCode.jl
@@ -0,0 +1,29 @@
+```
+Example from https://discourse.juliacn.com/t/topic/6289/10?u=mrs504aa
+```
+function ReadData(Chan::Channel)
+    while isopen(Chan)
+        S = readline()
+        if S == "exit"
+            close(Chan)
+        end
+        put!(Chan, S)
+    end
+    return 0
+end
+
+function Process(Chan::Channel)
+    while isopen(Chan)
+        S = take!(Chan)
+        println(S)
+    end
+end
+
+Chan = Channel{Any}(Inf)
+
+Threads.@spawn ReadData(Chan)
+Threads.@spawn Process(Chan)
+
+while isopen(Chan)
+    sleep(1.0)
+end
